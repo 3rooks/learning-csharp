6eec27ba7cbeebddd7b0dcce59cf1cd51861c6ff
3rooks <Brookslynx@gmail.com>
2023-04-18 14:14:23 -0300

    🚧 prompt user

--- src/Program.cs
+++ src/Program.cs
@@ -7,6 +7,21 @@ namespace MyConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
+
+            Console.WriteLine("Username:");
+            string? userName = Console.ReadLine();
+
+            Console.WriteLine("Age:");
+            string? ageString = Console.ReadLine();
+
+            if (int.TryParse(ageString, out int userAge))
+            {
+                Console.WriteLine($"Hello, {userName}! You are {userAge} years old.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid age value.");
+            }
         }
     }
 }
